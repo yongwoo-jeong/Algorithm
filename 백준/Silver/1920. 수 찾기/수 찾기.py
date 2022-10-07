4ab05d17db7b1c408e89c29e0e4f740659dcf527
yongwoo-jeong <80328674+yongwoo-jeong@users.noreply.github.com>
2022-10-07 18:29:53 +0900

    [Silver IV] Title: 수 찾기, Time: 164 ms, Memory: 52152 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -5,20 +5,10 @@ import bisect
 input = sys.stdin.readline
 
 n = int(input().strip())
-numbers = list(map(int,input().split()))
-numbers.sort()
+numbers = set(map(int,input().split()))
 
 answers = int(input().strip())
 cands = list(map(int,input().split()))
 
-for i in cands:
-    if i<numbers[0]:
-        print(0)
-    elif i > numbers[-1]:
-        print(0)
-    else:
-        bi = bisect.bisect_left(numbers, i)
-        if numbers[bi] == i:
-            print(1)
-        else:
-            print(0)
+for n in cands:
+    sys.stdout.write('1\n') if n in numbers else sys.stdout.write('0\n')
