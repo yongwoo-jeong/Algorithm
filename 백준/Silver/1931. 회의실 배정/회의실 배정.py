cad3b4bf606a7ea78babf0eaa6106c1249fd30cb
yongwoo-jeong <80328674+yongwoo-jeong@users.noreply.github.com>
2022-10-12 20:59:53 +0900

    [Silver I] Title: 회의실 배정, Time: 300 ms, Memory: 57760 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -4,26 +4,14 @@ input = sys.stdin.readline
 
 n = int(input())
 confs = [list(map(int, input().split())) for _ in range(n)]
-confs.sort(key= lambda x : (x[1],x[1]+x[0]))
+confs.sort(key= lambda x : (x[1], x[0]))
 
-holding = [confs[0]]
-confs = confs[1:]
+cnt = 1
 
-cnt = 0
-tmp = []
+end = confs[0][1]
+for i in range(1,n):
+    if confs[i][0] >= end:
+        cnt+=1
+        end = confs[i][1]
 
-for conf in confs:
-    start = conf[0]
-    end = conf[1]
-    if start >= holding[-1][1]:
-        holding.append(conf)
-    else:
-        tmp.append(conf)
-        cnt +=1
-        if cnt > 1 and end > tmp[-1][1]:
-            confs.pop()
-            confs += tmp
-            cnt = 0
-            tmp = []
-
-print(len(holding))
+print(cnt)
